1cf8167b7f9cf03f4d3a8a4475536e5a2469b702
sakshi2009 <sakshiandani20@gmail.com>
2021-08-29 18:20:13 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-pvw1tp
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-pvw1tp)
---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+import { products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  productsInside = products;
+  constructor() {}
+
+  ngOnInit() {}
+}
